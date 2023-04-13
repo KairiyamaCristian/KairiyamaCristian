22b7956e391bdc41025ea2994bd5af4c222dffe7
KairiyamaCristian <cristiankairiyama@gmail.com>
2023-04-13 20:02:40 -0300

    card con react

--- portafolio/src/main.jsx
+++ portafolio/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Card from './Card'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Card />
+
   </React.StrictMode>,
 )
