64e5af143b29d5f8dd01e3febaf4ab3d07fb255b
KairiyamaCristian <cristiankairiyama@gmail.com>
2023-04-14 22:44:18 -0300

    haciendo navbar

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -4,7 +4,32 @@ function App() {
 
   return (
     <>
-      <h1>algo</h1>
+      <nav className="navbar">
+          <ul className='nav__list'>
+            <a href="#" className='nav__home'>
+              <i className='home_img'></i>
+              <span className='home_text'>Home</span>
+            </a>
+
+
+            <a href="#" className='nav__skills'>
+              <i className='skills_img'></i>
+              <span className='skills_text'>skills</span>
+            </a>
+
+
+            <a href="#" className='nav__aboutme'>
+              <i className='aboutme_img'></i>
+              <span className='aboutme_text'>about me</span>
+            </a>
+
+
+            <a href="#" className='nav__contact'>
+              <i className='contact_img'></i>
+              <span className='contact_text'>contact</span>
+            </a>
+          </ul>
+      </nav>
     </>
   )
 }
